1540d7a42ccd0e703c6e2a78b088804637395860
Artur_artur <artursviridonov@gmail.com>
2019-05-29 04:01:53 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,201 +1,215 @@
-(function startAnimation() {
-  window.CanvasSlideshow = function(options) {
-    //  SCOPE
-    /// ---------------------------
-    var that = this;
+var itemCollection = document.querySelectorAll(".shown");
+var elem = itemCollection[0];
 
-    console.log(that);
-    //  OPTIONS
-    /// ---------------------------
-    options = options || {};
-    options.stageWidth = options.hasOwnProperty("stageWidth")
-      ? options.stageWidth
-      : document.documentElement.clientWidth;
-    options.stageHeight = options.hasOwnProperty("stageHeight")
-      ? options.stageHeight
-      : document.documentElement.clientHeight;
-    options.pixiSprites = options.hasOwnProperty("sprites")
-      ? options.sprites
-      : [];
-    options.centerSprites = options.hasOwnProperty("centerSprites")
-      ? options.centerSprites
-      : false;
-    options.autoPlay = options.hasOwnProperty("autoPlay")
-      ? options.autoPlay
-      : true;
-    options.autoPlaySpeed = options.hasOwnProperty("autoPlaySpeed")
-      ? options.autoPlaySpeed
-      : [10, 3];
-    options.fullScreen = options.hasOwnProperty("fullScreen")
-      ? options.fullScreen
-      : true;
-    options.displacementImage = options.hasOwnProperty("displacementImage")
-      ? options.displacementImage
-      : "";
-    options.displaceAutoFit = options.hasOwnProperty("displaceAutoFit")
-      ? options.displaceAutoFit
-      : false;
-    options.wacky = options.hasOwnProperty("wacky") ? options.wacky : false;
-    options.interactive = options.hasOwnProperty("interactive")
-      ? options.interactive
-      : false;
-    options.interactionEvent = options.hasOwnProperty("interactionEvent")
-      ? options.interactionEvent
-      : "";
-    options.displacementCenter = options.hasOwnProperty("displacementCenter")
-      ? options.displacementCenter
-      : false;
-    options.dispatchPointerOver = options.hasOwnProperty("dispatchPointerOver")
-      ? options.dispatchPointerOver
-      : false;
+window.onload = function() {
+  elem.classList.remove("shown");
 
-    //  PIXI VARIABLES
-    /// ---------------------------
-    var renderer = new PIXI.autoDetectRenderer(
-      options.stageWidth,
-      options.stageHeight,
-      { transparent: true }
-    );
-    var stage = new PIXI.Container();
-    var slidesContainer = new PIXI.Container();
-    var displacementSprite = new PIXI.Sprite.fromImage(
-      options.displacementImage
-    );
-    var displacementFilter = new PIXI.filters.DisplacementFilter(
-      displacementSprite
-    );
+  setTimeout(() => {
+    elem.classList.add("shown-second");
+    AOS.init();
+  }, 1900);
+};
 
-    /// ---------------------------
-    //  INITIALISE PIXI
-    /// ---------------------------
-    this.initPixi = function() {
-      // Add canvas to the HTML
-      //document.body.appendChild( renderer.view );
-      document.getElementById("js-canvas-wrapper").appendChild(renderer.view);
+setTimeout(function() {
+  (function startAnimation() {
+    window.CanvasSlideshow = function(options) {
+      //  SCOPE
+      /// ---------------------------
 
-      // Add child container to the main container
-      stage.addChild(slidesContainer);
+      var that = this;
 
-      // Enable Interactions
-      stage.interactive = true;
+      //  OPTIONS
+      /// ---------------------------
+      options = options || {};
+      // options.stageWidth = options.hasOwnProperty("stageWidth") ? options.stageWidth : 800;
+      // options.stageHeight = options.hasOwnProperty("stageHeight")
+      //   ? options.stageHeight
+      //   : 800;
+      options.pixiSprites = options.hasOwnProperty("sprites")
+        ? options.sprites
+        : [];
+      options.centerSprites = options.hasOwnProperty("centerSprites")
+        ? options.centerSprites
+        : false;
+      options.autoPlay = options.hasOwnProperty("autoPlay")
+        ? options.autoPlay
+        : true;
+      options.autoPlaySpeed = options.hasOwnProperty("autoPlaySpeed")
+        ? options.autoPlaySpeed
+        : [10, 3];
+      options.fullScreen = options.hasOwnProperty("fullScreen")
+        ? options.fullScreen
+        : true;
+      options.displacementImage = options.hasOwnProperty("displacementImage")
+        ? options.displacementImage
+        : "";
+      options.displaceAutoFit = options.hasOwnProperty("displaceAutoFit")
+        ? options.displaceAutoFit
+        : false;
+      options.wacky = options.hasOwnProperty("wacky") ? options.wacky : false;
+      options.interactive = options.hasOwnProperty("interactive")
+        ? options.interactive
+        : false;
+      options.interactionEvent = options.hasOwnProperty("interactionEvent")
+        ? options.interactionEvent
+        : "";
+      options.displacementCenter = options.hasOwnProperty("displacementCenter")
+        ? options.displacementCenter
+        : false;
+      options.dispatchPointerOver = options.hasOwnProperty(
+        "dispatchPointerOver"
+      )
+        ? options.dispatchPointerOver
+        : false;
 
-      // Fit renderer to the screen
-      if (options.fullScreen === true) {
-        renderer.view.style.objectFit = "cover";
-        renderer.view.style.width = "100%";
-        renderer.view.style.height = "100%";
-        // renderer.view.style.backgroundColor = "red";
-        // renderer.view.style.left = "50%";
-        renderer.view.style.webkitTransform = "scale(1)";
-        renderer.view.style.transform = "scale(1)";
-      } else {
-        renderer.view.style.maxWidth = "100%";
+      //  PIXI VARIABLES
+      /// ---------------------------
+      var renderer = new PIXI.autoDetectRenderer(
+        options.stageWidth,
+        options.stageHeight,
+        { transparent: true }
+      );
+      var stage = new PIXI.Container();
+      var slidesContainer = new PIXI.Container();
+      var displacementSprite = new PIXI.Sprite.fromImage(
+        options.displacementImage
+      );
+      var displacementFilter = new PIXI.filters.DisplacementFilter(
+        displacementSprite
+      );
 
-        // renderer.view.style.top = "50%";
-        // renderer.view.style.left = "50%";
-        renderer.view.style.webkitTransform = "translate( -50%, -50% )";
-        renderer.view.style.transform = "translate( -50%, -50% )";
-      }
+      /// ---------------------------
+      //  INITIALISE PIXI
+      /// ---------------------------
+      this.initPixi = function() {
+        // Add canvas to the HTML
+        //document.body.appendChild( renderer.view );
+        document.getElementById("js-canvas-wrapper").appendChild(renderer.view);
 
-      displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+        // Add child container to the main container
+        stage.addChild(slidesContainer);
 
-      // Set the filter to stage and set some default values for the animation
-      stage.filters = [displacementFilter];
+        // Enable Interactions
+        stage.interactive = true;
 
-      if (options.autoPlay === false) {
-        displacementFilter.scale.x = 0;
-        displacementFilter.scale.y = 0;
-      }
+        // Fit renderer to the screen
+        if (options.fullScreen === true) {
+          renderer.view.style.objectFit = "cover";
+          renderer.view.style.objectPosition = "center";
+          renderer.view.style.width = "100%";
+          renderer.view.style.height = "100%";
+          // renderer.view.style.backgroundColor = "red";
+          // renderer.view.style.left = "50%";
+          // renderer.view.style.webkitTransform = "scale(1)";
+          // renderer.view.style.transform = "scale(1)";
+        } else {
+          renderer.view.style.maxWidth = "100%";
 
-      if (options.wacky === true) {
-        displacementSprite.anchor.set(0.5);
-        displacementSprite.x = renderer.width / 2;
-        displacementSprite.y = renderer.height / 2;
-      }
-      // displacementFilter.style.backgroundColor = "rgba(0,0,0,1)";
-
-      displacementSprite.scale.x = 2;
-      displacementSprite.scale.y = 2;
-
-      // PIXI tries to fit the filter bounding box to the renderer so we optionally bypass
-      displacementFilter.autoFit = options.displaceAutoFit;
-
-      stage.addChild(displacementSprite);
-    };
-
-    /// ---------------------------
-    //  LOAD SLIDES TO CANVAS
-    /// ---------------------------
-    this.loadPixiSprites = function(sprites) {
-      var rSprites = options.sprites;
-
-      for (var i = 0; i < rSprites.length; i++) {
-        var texture = new PIXI.Texture.fromImage(sprites[i]);
-        var image = new PIXI.Sprite(texture);
-
-        if (options.centerSprites === true) {
-          image.anchor.set(0.5);
-          image.x = renderer.width / 2;
-          image.y = renderer.height / 2;
+          // renderer.view.style.top = "50%";
+          // renderer.view.style.left = "50%";
+          renderer.view.style.webkitTransform = "translate( -50%, -50% )";
+          renderer.view.style.transform = "translate( -50%, -50% )";
         }
 
-        slidesContainer.addChild(image);
+        displacementSprite.texture.baseTexture.wrapMode =
+          PIXI.WRAP_MODES.REPEAT;
+
+        // Set the filter to stage and set some default values for the animation
+        stage.filters = [displacementFilter];
+
+        if (options.autoPlay === false) {
+          displacementFilter.scale.x = 0;
+          displacementFilter.scale.y = 0;
+        }
+
+        if (options.wacky === true) {
+          displacementSprite.anchor.set(0.5);
+          displacementSprite.x = renderer.width / 2;
+          displacementSprite.y = renderer.height / 2;
+        }
+        // displacementFilter.style.backgroundColor = "rgba(0,0,0,1)";
+
+        displacementSprite.scale.x = 2;
+        displacementSprite.scale.y = 2;
+
+        // PIXI tries to fit the filter bounding box to the renderer so we optionally bypass
+        displacementFilter.autoFit = options.displaceAutoFit;
+
+        stage.addChild(displacementSprite);
+      };
+
+      /// ---------------------------
+      //  LOAD SLIDES TO CANVAS
+      /// ---------------------------
+      this.loadPixiSprites = function(sprites) {
+        var rSprites = options.sprites;
+
+        for (var i = 0; i < rSprites.length; i++) {
+          var texture = new PIXI.Texture.fromImage(sprites[i]);
+          var image = new PIXI.Sprite(texture);
+
+          if (options.centerSprites === true) {
+            image.anchor.set(0.5);
+            image.x = renderer.width / 2;
+            image.y = renderer.height / 2;
+          }
+
+          slidesContainer.addChild(image);
+        }
+      };
+
+      /// ---------------------------
+      //  DEFAULT RENDER/ANIMATION
+      /// ---------------------------
+      if (options.autoPlay === true) {
+        var ticker = new PIXI.ticker.Ticker();
+
+        ticker.autoStart = options.autoPlay;
+
+        ticker.add(function(delta) {
+          displacementSprite.x += options.autoPlaySpeed[0] * delta;
+          displacementSprite.y += options.autoPlaySpeed[1];
+          renderer.render(stage);
+        });
+      } else {
+        var render = new PIXI.ticker.Ticker();
+
+        render.autoStart = true;
+
+        render.add(function(delta) {
+          renderer.render(stage);
+        });
       }
+
+      /// ---------------------------
+      //  INIT FUNCTIONS
+      /// ---------------------------
+
+      this.init = function() {
+        that.initPixi();
+        that.loadPixiSprites(options.pixiSprites);
+      };
+
+      this.init();
     };
+  })();
 
-    /// ---------------------------
-    //  DEFAULT RENDER/ANIMATION
-    /// ---------------------------
-    if (options.autoPlay === true) {
-      var ticker = new PIXI.ticker.Ticker();
+  const svgPath = document.querySelectorAll(".path");
+  var spriteImagesSrc = [];
+  spriteImagesSrc.push("jungle.jpg");
 
-      ticker.autoStart = options.autoPlay;
-
-      ticker.add(function(delta) {
-        displacementSprite.x += options.autoPlaySpeed[0] * delta;
-        displacementSprite.y += options.autoPlaySpeed[1];
-        renderer.render(stage);
-      });
-    } else {
-      var render = new PIXI.ticker.Ticker();
-
-      render.autoStart = true;
-
-      render.add(function(delta) {
-        renderer.render(stage);
-      });
-    }
-
-    /// ---------------------------
-    //  INIT FUNCTIONS
-    /// ---------------------------
-
-    this.init = function() {
-      that.initPixi();
-      that.loadPixiSprites(options.pixiSprites);
-    };
-
-    this.init();
-  };
-})();
-
-const svgPath = document.querySelectorAll(".path");
-var spriteImagesSrc = [];
-spriteImagesSrc.push("jungle.jpg");
-
-var initCanvasSlideshow = new CanvasSlideshow({
-  sprites: spriteImagesSrc,
-  displacementImage:
-    "https://raw.githubusercontent.com/Pierrinho/elephant/master/pattern-clouds.jpg",
-  autoPlay: true,
-  centerSprites: true,
-
-  stageWidth: "",
-  stageHeight: "",
-  fullScreen: true,
-  autoPlaySpeed: [1, 0],
-  interactive: false,
-  interactionEvent: "click", // 'click', 'hover', 'both'
-  displaceAutoFit: true,
-  dispatchPointerOver: true // restarts pointerover event after click
-});
+  var initCanvasSlideshow = new CanvasSlideshow({
+    sprites: spriteImagesSrc,
+    displacementImage:
+      "https://raw.githubusercontent.com/Pierrinho/elephant/master/pattern-clouds.jpg",
+    autoPlay: true,
+    centerSprites: true,
+    fullScreen: true,
+    autoPlaySpeed: [1, 0],
+    interactive: true,
+    interactionEvent: "click", // 'click', 'hover', 'both'
+    displaceAutoFit: true,
+    displacementCenter: true,
+    dispatchPointerOver: false // restarts pointerover event after click
+  });
+}, 1000);
